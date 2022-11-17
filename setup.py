b6391f59c442c5e497a8fd260c4bff59cce77967
Mike Doran <mikrdoran@gmail.com>
2022-11-17 12:11:59 +0000

    bump tf version

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
     name="easy_tensorflow_models",
     packages=["easy_tensorflow_models"],
-    version="0.1.5",
+    version="0.2.0",
     description="Helpers for creating, training, and serving TF models",
     author="Michael Doran",
     author_email="mikrdoran@gmail.com",
     url="https://github.com/miksyr/easy_tensorflow_models",
     keywords=["tensorflow"],
-    install_requires=["tensorflow==2.7.1", "tqdm>=4.38.0"],
+    install_requires=["tensorflow==2.10.1", "tqdm>=4.38.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
